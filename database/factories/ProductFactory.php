466462d1ee00755fa397816d7ca2d714aca55295
Arefin21 <arefinshagor18@gmail.com>
2025-12-12 01:54:35 +0600

    feat: add Product model with migration, factory and seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+{
+    return [
+        'name' => $this->faker->word(),
+        'sku' => strtoupper(\Illuminate\Support\Str::random(8)),
+        'price' => $this->faker->randomFloat(2, 10, 500),
+        'stock' => $this->faker->numberBetween(1, 100),
+        'description' => $this->faker->sentence(),
+        'image' => 'https://picsum.photos/300/300?random=' . $this->faker->unique()->numberBetween(1, 100),
+
+    ];
+}
+
+}
